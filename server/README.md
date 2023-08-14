8a09ae616960f2434ed41e742267b2331e2066da
hindawi <ahmedhindawi2001@gmail.com>
2023-08-14 08:24:28 +0300

    init the app server

---
+++ server/README.md
@@ -0,0 +1,5 @@
+# cmds
+
+- run fastapi server : `uvicorn main:app --reload`
+- link http://127.0.0.1:8000
+
--- server/main.py
+++ server/main.py
@@ -1,15 +1,9 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from typing import Annotated
 
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/")
+async def engine(input: Annotated[str, Form(...)]):
+    return {"input": input}
